be7c1aa8bd39bab93f93117620a3fee55d691121
iznauy <zy05160516@126.com>
2019-02-26 15:32:02 +0800

    对创建发布页面进行精修

--- src/components/teacher/course/ReleaseCreateTable.vue
+++ src/components/teacher/course/ReleaseCreateTable.vue
@@ -1,36 +1,34 @@
 <template>
   <div>
-    <el-dialog :visible="true" :before-close="close" title="发布课程">
-      <el-form :model="form">
-        <el-form-item label="课程时间">
-          <el-col :span="7">
-            <el-date-picker type="date" placeholder="开始日期" v-model="form.beginDate" style="width: 100%;"></el-date-picker>
+    <el-dialog :visible="true" :before-close="close" title="发布课程" width="40%">
+      <el-form :model="form" style="width: 90%; margin: 5px auto" label-position="left">
+        <el-form-item label="课程时间" label-width="80px">
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="开始日期" v-model="form.beginDate" size="small" style="width: 100%;"></el-date-picker>
           </el-col>
           <el-col class="line" :span="2">-</el-col>
-          <el-col :span="7">
-            <el-date-picker type="date" placeholder="结束日期" v-model="form.endDate" style="width: 100%;"></el-date-picker>
+          <el-col :span="11">
+            <el-date-picker type="date" placeholder="结束日期" v-model="form.endDate" size="small" style="width: 100%;"></el-date-picker>
           </el-col>
         </el-form-item>
-        <el-form-item label="班次">
-          <el-col :span="7">
-            <el-input v-model="form.classOrder"></el-input>
-          </el-col>
+        <el-form-item label="班次" label-width="80px">
+            <el-input v-model="form.classOrder" size="small" type="text"></el-input>
         </el-form-item>
-        <el-form-item label="是否限选">
-          <el-col :span="3">
+        <el-form-item label="是否限选" label-width="80px">
+          <el-col :span="8">
             <el-switch v-model="form.hasQuota"></el-switch>
           </el-col>
         </el-form-item>
-        <el-form-item label="限额" v-if="form.hasQuota">
-          <el-col :span="7">
+        <el-form-item label="限额" v-if="form.hasQuota" label-width="80px" size="small">
+          <el-col>
             <el-input v-model="form.quota"></el-input>
           </el-col>
         </el-form-item>
+        <el-form-item>
+          <el-button @click="close" size="small">取 消</el-button>
+          <el-button type="primary" @click="createRelease" size="small">确 定</el-button>
+        </el-form-item>
       </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="close">取 消</el-button>
-        <el-button type="primary" @click="createRelease">确 定</el-button>
-      </div>
     </el-dialog>
   </div>
 </template>
